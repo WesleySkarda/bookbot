cf2c36acd8996224742d28b0f47402bb420cdc5b
WesleySkarda <Wesleykimballskarda@gmail.com>
2025-04-13 19:48:56 -0600

    Updated main and stats to fit with the project

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import *
+import sys
 
 def get_book_text(filepath):
     with open(filepath) as book:
@@ -6,9 +7,23 @@ def get_book_text(filepath):
     return book_contents
 
 def main():
-    filepath = "books/frankenstein.txt"
+    try:
+        filepath = sys.argv[1]
+    except Exception as e:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
     text = get_book_text(filepath)
-    print(number_of_words(text))
+    number_of_words_var = number_of_words(text)
+    repeting_character_var = sort_dictonary(repeting_character(text))
+    print('============ BOOKBOT ============')
+    print(f"Analyzing book found at {filepath}")
+    print("----------- Word Count ----------")
+    print(number_of_words_var)
+    print("--------- Character Count -------")
+    for item in repeting_character_var:
+        if item.isalpha():
+            print(f"{item}: {repeting_character_var[item]}")
+    print("============= END ===============")
 
 if __name__ == "__main__":
     main()
--- stats.py
+++ stats.py
@@ -1,2 +1,17 @@
 def number_of_words(text):
-    return f"{len(text.split())} words found in the document"
+    return f"Found {len(text.split())} total words"
+
+def repeting_character(text):
+    character_dict = {}
+    for letter in text.lower():
+        if letter in character_dict:
+            character_dict[letter] += 1
+        else:
+            character_dict[letter] = 1
+    del character_dict[' ']
+    del character_dict['\n']
+    return character_dict
+
+def sort_dictonary(dictonary):
+    return dict(sorted(dictonary.items(), key=lambda item: item[1], reverse=True))
+    
